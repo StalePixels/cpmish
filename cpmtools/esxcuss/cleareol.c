99f4148fe997ade660b9d17e4fd59853a1130d3b
D. Rimron-Soutter <darran@xalior.com>
2020-10-08 23:03:50 +0100

    Clear to end of line UI done

--- cpmtools/esxcuss/cleareol.c
+++ cpmtools/esxcuss/cleareol.c
@@ -4,18 +4,20 @@
  */
 
 #include "libcuss.h"
+#include "textmode.h"
 
 void con_clear_to_eol(void)
 {
     if (screeny >= SCREENHEIGHT)
             return;
 
-    uint16_t i = SCREENWIDTH - screenx - 1;
+    uint16_t i = SCREENWIDTH - screenx;
     if ((i != 0) && (screeny == (SCREENHEIGHT-1)))
         i--;
 
     while (i--)
-        con_putc(' ');
+        tilemap[screeny][screenx].tile = ' ';
+
     con_goto(screenx, screeny);
 
 }
